c910986c598dcd8b991d933266b4eadd2c2f3e92
FERNANDOJUNIOR06 <F.JUNIOR06@escola.pr.gov.br>
2025-07-07 17:17:32 -0300

    Add files via upload

---
+++ mysql.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE IF NOT EXISTS FastFood;
+USE FastFood;
+
+
+CREATE TABLE IF NOT EXISTS Clientes (
+    idClientes INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    NomeClientes VARCHAR(100) NOT NULL,
+    Telefone VARCHAR(20),
+    Email VARCHAR(100) NOT NULL,
+    Endereco VARCHAR(250) NOT NULL
+);
+
+
+INSERT INTO Clientes (NomeClientes, Telefone, Email, Endereco) VALUES 
+('Augusto','45 99769-6578','augusto2006@gmail.com','Rua Otávio Portes 2765'),
+('Bruno','45 99969-5578','bruno.do.grau@gmail.com','Rua Francisco Fernandes 1495'),
+('Fernando','45 99799-5418','fernandinho@gmail.com','Rua Conceição da Maria 5675'),
+('Henrique','45 99976-1083','henrique.silva09@gmail.com','Rua Catanduva Mendes 4927'),
+('Natan','45 99779-6578','natan@gmail.com','Rua Constantino bananinha 7777'),
+('Pedro','45 98997-5868','pedrinho@gmail.com','Rua mandinga da cruz 9628'),
+('Daniel','45 99829-6118','danielzinhofado2002@gmail.com','Rua Javier dos santos 7775');
+
+-- Tabela Produtos
+CREATE TABLE IF NOT EXISTS Produtos (
+    idProdutos BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    NomeProduto VARCHAR(150) NOT NULL,
+    preco DECIMAL(6,2) NOT NULL
+);
+
+
+INSERT INTO Produtos (NomeProduto, preco) VALUES
+('X-Burguer', 15.90),
+('X-Salada', 16.50),
+('Refrigerante 350ml', 6.00),
+('Batata Frita Média', 9.50),
+('Milkshake Chocolate 500ml', 12.00),
+('Combo X-Tudo + Refri', 28.90),
+('Cheeseburguer Vegano', 17.80);
+
+
+CREATE TABLE IF NOT EXISTS Pedidos (
+    idPedidos INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    idCliente INT NOT NULL,
+    idProduto BIGINT NOT NULL,
+    status ENUM('Pronto', 'Em preparo', 'Cancelado', 'Entregue'),
+    FOREIGN KEY (idCliente) REFERENCES Clientes(idClientes),
+    FOREIGN KEY (idProduto) REFERENCES Produtos(idProdutos)
+);
+
+
+INSERT INTO Pedidos (idCliente, idProduto, status) VALUES
+(1, 1, 'Entregue'),          -- Augusto comprou X-Burguer
+(2, 3, 'Pronto'),            -- Bruno comprou Refrigerante
+(3, 4, 'Em preparo'),        -- Fernando comprou Batata Frita
+(4, 2, 'Entregue'),          -- Henrique comprou X-Salada
+(5, 6, 'Cancelado'),         -- Natan comprou Combo X-Tudo + Refri
+(6, 5, 'Entregue'),          -- Pedro comprou Milkshake
+(7, 7, 'Em preparo'),        -- Daniel comprou Cheeseburguer Vegano
+(1, 6, 'Entregue'),          -- Augusto fez outro pedido: Combo
+(3, 2, 'Pronto');            -- Fernando fez outro pedido: X-Salada
